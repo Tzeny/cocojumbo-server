55eb3a35dbc8f8bc1c91c399fd61117ed1d0d710
Andrei Tenescu <teny96@gmail.com>
2017-10-31 17:51:12 +0200

    Tested quickstart example Created our own models

--- Cocojumbo/settings.py
+++ Cocojumbo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cocojumbo_server.apps.CocojumboServerConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
 
 ROOT_URLCONF = 'Cocojumbo.urls'
 
--- cocojumbo_server/models.py
+++ cocojumbo_server/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Camera(models.Model):
+    name = models.TextField()
+
+
+class User(models.Model):
+    username = models.TextField()
+    password = models.TextField()
+    email = models.TextField()
+    cameras = models.ManyToManyField(Camera)
+
+
+class Alert(models.Model):
+    timestamp = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User)
+    message = models.TextField()
