66cdf55d6fdbe5a2bf083a3bad97eccf1c1a4544
Andrei Tenescu <teny96@gmail.com>
2017-10-31 18:23:12 +0200

    Created UserSerializer using serializers.Serializer class, updating in the next commit OH-19

--- cocojumbo_server/serializers.py
+++ cocojumbo_server/serializers.py
@@ -1,14 +1,26 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from cocojumbo_server.models import User,Camera,Alert
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'phone', 'camera_urls')
+class CameraSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+    cameras = CameraSerializer(many=True, required=False)
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username',instance.username)
+        instance.password = validated_data.get('password',instance.password)
+        instance.email = validated_data.get('password',instance.email)
+        instance.cameras = validated_data.get('cameras', instance.cameras)
+
+        instance.save()
+        return instance
